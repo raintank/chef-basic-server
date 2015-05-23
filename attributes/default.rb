dfa35d416a3d4bb4723c52a5f87e24b2676d6475
ctdk <jbingham@gmail.com>
2015-05-22 17:13:09 -0700

    Basic server config cookbook, only covers user management so far.

---
+++ attributes/default.rb
@@ -0,0 +1,2 @@
+default['authorization']['sudo']['passwordless'] = true
+default['authorization']['sudo']['groups'] = [ 'sysadmin' ]
---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'basic_server'
+maintainer       'Raintank Inc.'
+maintainer_email 'jeremy@raintank.io'
+license          'Apache 2.0'
+description      'Installs/Configures basic-server'
+long_description 'Installs/Configures basic-server'
+version          '0.1.0'
+
+depends "sudo", '~> 2.7.1'
